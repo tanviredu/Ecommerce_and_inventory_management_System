ebe4a3708822fc9d02c6f2df5b3bbd48a3dcf485
tanviredu <tanviredu2018@gmail.com>
2020-07-26 18:18:21 +0600

    added

--- inventory_management_system/InventoryManagement/inventory/views.py
+++ inventory_management_system/InventoryManagement/inventory/views.py
@@ -3,7 +3,7 @@ from .models import Laptop,Desktop,Mobile
 from .forms import LaptopForm,DesktopForm,MobileForm
 from django.urls import reverse,reverse_lazy
 from django.shortcuts import redirect
-from django.views.generic import CreateView,ListView,UpdateView,DetailView
+from django.views.generic import CreateView,ListView,UpdateView,DeleteView
 
 def index(request):
     #return render(request,'inventory/index.html')
@@ -49,11 +49,40 @@ class CreateMobileView(CreateView):
     template_name = "inventory/create_item.html"
 
 
+class EditMobileView(UpdateView):
+    form_class = MobileForm
+    template_name = "inventory/edit_form.html"
+    model = Mobile
+    
+class DeleteMobileView(DeleteView):
+    model = Mobile
+    success_url = reverse_lazy('inv:mobiles')
+
+
 class CreateLaptopView(CreateView):
     form_class = LaptopForm
     template_name = "inventory/create_item.html"
+
+class EditlaptopView(UpdateView):
+    form_class = LaptopForm
+    template_name = "inventory/edit_form.html"
+    model = Laptop
+    
+class DeletelaptopView(DeleteView):
+    model = Laptop
+    success_url = reverse_lazy('inv:laptops')
 
 
 class CreateDesktopView(CreateView):
     form_class = DesktopForm
     template_name = "inventory/create_item.html"
+
+
+class EditDesktopView(UpdateView):
+    form_class = DesktopForm
+    template_name = "inventory/edit_form.html"
+    model = Desktop
+
+class DeleteDesktopsView(DeleteView):
+    model = Desktop
+    success_url = reverse_lazy('inv:desktops')
